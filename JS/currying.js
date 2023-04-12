03f2d791a02729510fdc6627eec346a823e8f700
Abhishek Gupta 🎸 <abhishekgupta@infeedo.com>
2023-04-12 09:14:46 +0530

    #1: Currying

--- JS/currying.js
+++ JS/currying.js
@@ -3,5 +3,13 @@
 use 'strict';
 
 function currying (fn = function(){}) {
-    
+    return function(...args) {
+        if (args.length >= fn.length) {
+            return fn.apply(this, args);
+        } else {
+            return function (...args1) {
+                return currying.apply(this, args.concat(args1))
+            }
+        }
+    }
 }
